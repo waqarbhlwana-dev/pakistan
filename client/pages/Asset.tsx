c99e5154b5a50c004d91c9cd4b3b056179e19adf
waqarbhlwana-dev <waqarbhlwana@gmail.com>
2025-12-12 09:30:39 +0500

    Update Asset.tsx

--- client/pages/Asset.tsx
+++ client/pages/Asset.tsx
@@ -14,7 +14,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-const ASSET_ACCESS_CODE = "bhulwal";
+const ASSET_ACCESS_CODE = "98sb";
 
 export default function AssetPage() {
   const { symbol } = useParams<{ symbol: string }>();
