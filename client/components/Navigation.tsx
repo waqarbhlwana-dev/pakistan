48f67c1d773167ae552ff2331b0a11b44e8ca2d4
waqarbhlwana-dev <waqarbhlwana@gmail.com>
2026-02-06 11:56:11 +0500

    Update Navigation.tsx

--- client/components/Navigation.tsx
+++ client/components/Navigation.tsx
@@ -28,7 +28,7 @@ export default function Navigation() {
     "MARI","MEHT","MUREB","MCB","MEBL","MLCF","MTL","NBP","NCL","NESTLE",
     "NML","NATF","NWPL","OGDC","OIL","PAP","PAKT","PAEL","PIBTL","PIA","PLHC","PTC",
     "POL","POIC","POWER","PPL","PREL","PSMC","PSEL","PSO","PSX","PGLC","PUL","PKGP",
-    "RMPL","SHELL","SSOM","SHEL","SYS","SHFA","SNGP","SSGC","SIEMENS","TASS",
+    "RMPL","SHELL","SSOM","SHEL","SEARL","SYS","SHFA","SNGP","SSGC","SIEMENS","TASS",
     "TOMCL","TRG","THALL","TPLRFF1","TVMH","UBL","UPFL","UNITY","WADT",
     "WAVESEED","ZADCO","ZONG","ZTBL"
   ];
