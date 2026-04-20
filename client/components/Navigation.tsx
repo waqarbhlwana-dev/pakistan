fb6efb04e54f9a93645e631dce75b34f7773aafb
waqarbhlwana-dev <waqarbhlwana@gmail.com>
2026-04-20 06:59:27 +0500

    Update Navigation.tsx

--- client/components/Navigation.tsx
+++ client/components/Navigation.tsx
@@ -30,7 +30,7 @@ export default function Navigation() {
     "POL","PIOC","POWER","PPL","PREL","PSMC","PSEL","PSO","PSX","PGLC","PUL","PKGP",
     "RMPL","SHELL","SSOM","SAZEW","SHEL","SEARL","SYS","SHFA","SNGP","SSGC","SIEMENS","TASS",
     "TOMCL","TRG","TGL","THALL","TPLRFF1","TVMH","UBL","UPFL","UNITY","WADT",
-    "WAVESEED","YOUW","ZONG","ZTBL"
+    "WAVESEED","WTL","YOUW","ZONG","ZTBL"
   ];
 
   const otherPsxSymbols = ["AVN","PIBT","CENERGY"];
